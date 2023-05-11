942df8eedf6980bb6f69fdff52a0c4aabdb75721
Nicholas Chan <99701277+nicknahc@users.noreply.github.com>
2023-05-11 01:48:31 -0700

    soundboard updated to trigger repeat

--- client/src/instruments/SoundBoard.tsx
+++ client/src/instruments/SoundBoard.tsx
@@ -1,8 +1,8 @@
 // 3rd party library imports
 import * as Tone from 'tone';
 import classNames from 'classnames';
-import { List, Range } from 'immutable';
-import { useState } from 'react';
+import { List } from 'immutable';
+import { useEffect, useState} from 'react';
 
 // project imports
 import { Instrument, InstrumentProps } from '../Instruments';
@@ -14,26 +14,68 @@ import { Instrument, InstrumentProps } from '../Instruments';
 interface SoundButtonProps {
   note: string; // e.g. C1, D#2, Gb3, etc.
   synth?: Tone.Synth;
-//  index: number;
 }
 
 export function SoundButton({
   note,
   synth,
 }: SoundButtonProps): JSX.Element {
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const playNote = (repeatCount: number) => {
+    setIsPlaying(true);
+    if (synth) {
+      synth.triggerAttackRelease(note, "8n");
+      setTimeout(() => {
+        if (repeatCount > 1) {
+          playNote(repeatCount - 1);
+        } else {
+          setIsPlaying(false);
+        }
+      }, 500); // Adjust the duration between repeats (in milliseconds)
+    }
+  };
+
+  const stopNote = () => {
+    setTimeout(() => {
+      if (!isPlaying && synth) {
+        synth.triggerRelease();
+      }
+    }, 0);
+  };
+
+  useEffect(() => {
+    const handleMouseLeave = () => {
+      setTimeout(() => {
+        if (!isPlaying && synth) {
+          synth.triggerRelease();
+        }
+      }, 0);
+    };
+
+    return () => {
+      handleMouseLeave();
+    };
+  }, [isPlaying, note, synth]);
+
   return (
     <button
-      onMouseDown={() => synth?.triggerAttack(`${note}`)}
-      onMouseUp={() => synth?.triggerRelease('+0.25')}
-      //className={'ba pointer absolute dim'}
+      onMouseDown={() => playNote(4)} // Repeat the note 4 times
+      onMouseUp={stopNote}
+      onMouseLeave={stopNote}
+      className={classNames({
+        playing: isPlaying,
+      })}
       style={{
-        width: '30px',  // set the width of the button
-        height: '30px', // set the height of the button
+        width: '30px',
+        height: '30px',
         borderRadius: '10px',
       }}
     />
   );
 }
+
+
 
 function ButtonType({ title, onClick, active }: any): JSX.Element {
   return (
@@ -49,28 +91,14 @@ function ButtonType({ title, onClick, active }: any): JSX.Element {
   );
 }
 
-function SoundBoard({ synth, setSynth }: InstrumentProps): JSX.Element {
+function SoundBoard({ synth, setSynth}: InstrumentProps): JSX.Element {
+
+  
+
   const keys = ['C', 'D', 'E', 'G'];
-  const octaves = [1, 2, 3, 4];
+  const octaves = [4, 3, 2, 1];
   const grid = Array.from({ length: octaves.length }, () => new Array(keys.length).fill(false));
 
-  //const synth = new Tone.Synth().toDestination();
-
-  const setOscillator = (newType: Tone.ToneOscillatorType) => {
-    setSynth(oldSynth => {
-      oldSynth.disconnect();
-
-      const newSynth = new Tone.Synth({
-        oscillator: { type: newType } as Tone.OmniOscillatorOptions,
-      });
-
-      const distortion = new Tone.Distortion(0.8);
-      newSynth.connect(distortion);
-      distortion.connect(Tone.Destination);
-
-      return newSynth;
-    });
-  };
 
   const oscillators: List<OscillatorType> = List([
     'sine',
@@ -84,6 +112,22 @@ function SoundBoard({ synth, setSynth }: InstrumentProps): JSX.Element {
     'amsawtooth',
     'amtriangle',
   ]) as List<OscillatorType>;
+
+  const setOscillator = (newType: Tone.ToneOscillatorType) => {
+    console.log(newType);
+
+    setSynth((oldSynth: any) => {
+      oldSynth.disconnect();
+
+      return new Tone.MembraneSynth({
+        oscillator: { type: newType } as Tone.OmniOscillatorOptions,
+      }).toDestination() as any;
+    });
+  };
+
+  useEffect(() => {
+    setOscillator('square');
+  }, []); 
 
   return (
     <div className="pv4">
@@ -106,7 +150,7 @@ function SoundBoard({ synth, setSynth }: InstrumentProps): JSX.Element {
             key={o}
             title={o}
             onClick={() => setOscillator(o)}
-            active={synth?.oscillator.type === o}
+            active={synth && synth.get().oscillator.type === o}
           />
         ))}
       </div>
