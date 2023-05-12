9c311d3b768442977d8262630d73a0c2992b661e
Kilian Kistenbroker <68315043+KilianKistenbroker@users.noreply.github.com>
2023-05-12 12:08:19 -0700

    merged from slider-instrument and added song to schema

--- server/src/sql/schema.sql
+++ server/src/sql/schema.sql
@@ -21,3 +21,8 @@ INSERT INTO songs (id, song_title, bpm, notes) --by Felix Colgrave: https://yout
 VALUES (4, 'The Royal crab''s Off-Screen Journey to the Ocean floor', 105, 'F5/4n. C5/4n. A5/4n F5/4n. C5/4n. C5/8n D5/8n Eb5/8n D5/8n Eb5/8n D5/8n Eb5/4n Eb5/8n D5/8n C5/1n F5/4n. C5/4n. A5/4n F5/4n. C5/4n. C5/8n D5/8n E5/8n E5/8n D5/8n E5/4n E5/8n F5/4n C6/4n G5/4n C5/2n D6/8n D6/8n C6/8n D6/4n Bb5/4n. C6/8n C6/8n Bb5/8n C6/4n A5/4n. Bb5/4n A5/4n G5/4n F5/4n E5/4n. C5/4n.. F5/4n. C5/4n. D6/8n Bb5/8n C6/4n D6/4n A5/4n E5/8n D5/8n C5/8n F5/8n G5/8n A5/4n G5/8n E5/4n F5/2n');
 INSERT INTO songs (id, song_title, bpm, notes) --by J.S. Bach: https://www.youtube.com/watch?v=5AFfCY1Qp24
 VALUES (5, 'Contrapunctus IX (shortened)', 110, '0/4n D4/4n D5/2n. Db5/8n B4/8n A4/8n G4/8n F4/8n E4/8n D4/8n Db4/8n D4/8n E4/8n F4/8n D4/8n E4/8n f4/8n g4/8n gb4/8n g4/8n a4/8n bb4/8n g4/8n a4/8n bb4/8n bb4/16n c5/16n bb4/16n a4/4n. a4/8n g4/8n a4/8n bb4/8n d5/8n c5/8n bb4/8n c5/8n bb4/8n a4/8n g4/8n f4/8n bb4/8n a4/8n g4/8n a4/8n g4/8n f4/8n e4/8n d4/8n g4/8n f4/8n e4/8n f4/8n e4/8n d4/8n c4/8n b3/4n e4/4n ab4/4n e4/4n c4/8n d4/8n c4/8n b3/8n a3/4n bb4/4n f4/8n g4/8n f4/8n e4/8n d4/4n d5/4n fb4/4n g4/8n b4/8n c5/4n bb4/4n a4/2n. ab4/4n a4/2n. e4/4n f4/2n. f4/4n e4/2n. g4/2n f4/8n g4/8n a4/2n d4/2n. g4/4n e4/4n f4/8n g4/8n f4/4n e4/4n d4/2n');
+INSERT INTO songs (id, song_title, bpm, notes)
+VALUES (6, 'Toccata and Fugue in Dm, BWV 565 (Simplified)', 80, 
+'D4/10n A3/10n D5/2n F5/10n A5/10n D5/10n A3/10n D4/2n A3/2n D5/10n F5/10n A5/2n D5/4n A3/4n D4/4n A4/2n D4/10n F4/10n A4/2n D5/10n F4/10n A4/10n F4/10n D4/2n');
+
+
