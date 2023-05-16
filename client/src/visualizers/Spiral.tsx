007b6e3963c6b7566ebe688a26b323b1b89cd3ab
Nicholas Chan <99701277+nicknahc@users.noreply.github.com>
2023-05-16 00:28:21 -0700

    minor comments

--- client/src/visualizers/Spiral.tsx
+++ client/src/visualizers/Spiral.tsx
@@ -9,7 +9,7 @@ export const SpiralVisualizer = new Visualizer(
     const width = window.innerWidth;
     const height = window.innerHeight / 2;
 
-    p5.colorMode(p5.HSB, 360, 100, 100); // set color mode to HSB
+    p5.colorMode(p5.HSB, 360, 100, 100);
 
     p5.background(0, 0, 0, 255);
     p5.noFill();
@@ -17,7 +17,7 @@ export const SpiralVisualizer = new Visualizer(
     let r = 0;
     const values = analyzer.getValue();
 
-    p5.translate(width / 2, height / 2); // Move the origin to the center of the canvas
+    p5.translate(width / 2, height / 2);
 
     // Calculate the average amplitude
     let sum = 0;
@@ -26,8 +26,8 @@ export const SpiralVisualizer = new Visualizer(
     }
     const averageAmplitude = 20 * sum / values.length;
 
-    const rotationSpeed = p5.map(averageAmplitude, 0, 1, 0.01, 0.1); // Adjust the range and speed as needed
-    p5.rotate(p5.frameCount * rotationSpeed); // Rotate the entire visualization based on frame count and average amplitude
+    const rotationSpeed = p5.map(averageAmplitude, 0, 1, 0.01, 0.1);
+    p5.rotate(p5.frameCount * rotationSpeed);
 
     for (let i = 0; i < values.length; i += 3) {
       const amplitude = values[i] as number;
@@ -36,7 +36,7 @@ export const SpiralVisualizer = new Visualizer(
       const y = r * p5.sin(angle);
       let circleSize = 1 + (amplitude * height) / 3;
 
-      const hue = (p5.map(x, -width / 2, width / 2, 200 , 300 + amplitude * p5.frameCount * 10) ) % 360; // Vary the hue over time
+      const hue = (p5.map(x, -width / 2, width / 2, 200 , 300 + amplitude * p5.frameCount * 10) ) % 360;
       const saturation = p5.map(y, -height / 2, height / 2, 70, 80);
       const brightness = p5.map(amplitude, 0, 1, 60, 100);
       const color = p5.color(hue, saturation, brightness);
@@ -45,9 +45,9 @@ export const SpiralVisualizer = new Visualizer(
       p5.strokeWeight(4);
       p5.circle(x, y, circleSize);
 
-      // wiggle-like effect
-      const wiggleSize = circleSize * 0.25; // Size of the wiggle-like motion
-      const wiggleOffset = p5.map(amplitude, 0, 1, -wiggleSize, wiggleSize); // Offset based on the amplitude
+      // wiggle effect
+      const wiggleSize = circleSize * 0.25;
+      const wiggleOffset = p5.map(amplitude, 0, 1, -wiggleSize, wiggleSize);
       const wiggleX = x + wiggleOffset;
       const wiggleY = y + wiggleOffset;
 
